eb040723d2d3646fd56c7952449dabad7deb50b3
flxplz <felix.pelaez.marza@gmail.com>
2022-12-27 21:08:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ mv /etc/apk/repositories /etc/apk/repositories.bak
 echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/main" >> /etc/apk/repositories
 exho "http://dl-cdn.alpinelinux.org/alpine/latest-stable/community" >> /etc/apk/repositories
 apk update && apk add git sudo docker docker-compose nano xhost
+setup-xorg-base
 rc-update add docker boot
